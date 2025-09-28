c1fdcbdd1b004013b69d0ff28fed79103bf1f4e4
pareshacharya <paresh.acharya10@gmail.com>
2025-09-28 11:14:48 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,30 +1,39 @@
-
 terraform {
-  required_version = ">= 0.12"
+  # Terraform CLI version
+  required_version = ">= 1.5.0"
+
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"   # v6.x since you don't want to downgrade
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = ">= 2.30"  # latest 2.x line
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"   # latest 3.x
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.1.0"
+      version = "~> 2.5"   # latest 2.x
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = "~> 3.2"   # latest 3.x
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 2.3"   # latest 2.x
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"   # latest 4.x
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"  # latest 0.13.x
     }
   }
 }
